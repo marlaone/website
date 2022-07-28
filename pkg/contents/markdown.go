3cb87e8fb921fd6b880f12d0a920b440382b0d19
MrParano1d <jps@marla.one>
2022-07-28 21:31:10 +0200

    feat(contents): :zap: added naive parser cache

--- pkg/contents/markdown.go
+++ pkg/contents/markdown.go
@@ -3,17 +3,44 @@ package contents
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/yuin/goldmark"
 	meta "github.com/yuin/goldmark-meta"
 	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer/html"
-	"io/ioutil"
-	"os"
-	"regexp"
-	"strings"
 )
+
+type ParserCacheItem struct {
+	Content []byte
+	Meta    MDMeta
+}
+type ParserCache map[string]*ParserCacheItem
+
+func (m ParserCache) Exists(key string) bool {
+	_, ok := m[key]
+	return ok
+}
+
+func (m ParserCache) Set(key string, content []byte, meta MDMeta) {
+	m[key] = &ParserCacheItem{
+		Content: content,
+		Meta:    meta,
+	}
+}
+
+func (m ParserCache) Get(key string) *ParserCacheItem {
+	ci, ok := m[key]
+	if ok {
+		return ci
+	}
+	return nil
+}
 
 type SEOMeta struct {
 	Title       string
@@ -67,10 +94,14 @@ func MDMetaFromMap(meta map[string]interface{}) MDMeta {
 type Parser struct {
 	md        goldmark.Markdown
 	sanitizer *bluemonday.Policy
+	cache     ParserCache
 }
 
 func NewParser() *Parser {
-	p := &Parser{}
+	p := &Parser{
+		// @TODO implement real cache with check for modification time or expiry
+		cache: ParserCache{},
+	}
 	p.md = goldmark.New(
 		goldmark.WithExtensions(
 			extension.GFM,
@@ -93,6 +124,11 @@ func (p *Parser) Parse(markdownPath string) ([]byte, MDMeta, error) {
 	var buf bytes.Buffer
 	var metaData MDMeta
 
+	if p.cache.Exists(markdownPath) {
+		item := p.cache.Get(markdownPath)
+		return item.Content, item.Meta, nil
+	}
+
 	f, err := os.Open(markdownPath)
 	if err != nil {
 		return nil, metaData, fmt.Errorf("[contents/Parser] failed to open file: %v", err)
@@ -113,6 +149,8 @@ func (p *Parser) Parse(markdownPath string) ([]byte, MDMeta, error) {
 	metaData = MDMetaFromMap(meta.Get(context))
 
 	sanitized := p.sanitizer.SanitizeBytes(buf.Bytes())
+
+	p.cache.Set(markdownPath, sanitized, metaData)
 
 	return sanitized, metaData, nil
 }
